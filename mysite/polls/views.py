075feec053200f7a95e66e164055a6ebf838283e
Mehsheed Ahmed Syed Abdul <mehsheed59@gmail.com>
2023-10-18 16:47:37 -0600

    feat: heroku

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,22 +7,22 @@ from .models import Choice, Question
 
 
 class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.order_by("-pub_date")[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
-    template_name = 'polls/detail.html'
+    template_name = "polls/detail.html"
 
 
 class ResultsView(generic.DetailView):
     model = Question
-    template_name = 'polls/results.html'
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
